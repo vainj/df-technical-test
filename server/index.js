33c30d71e609cc4ef70dff312fb07c05b8b30919
jeremyv <jeremy.vain@gmail.com>
2020-02-13 17:02:17 +0100

    Creates GraphQL example server.

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {ApolloServer, gql} from "apollo-server-express";
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+    Query : {
+        hello : () => 'Hello world!',
+    },
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+const app = express();
+server.applyMiddleware({app});
+
+// Listen for connections on given port
+app.listen({port : process.env.PORT || 4000});
