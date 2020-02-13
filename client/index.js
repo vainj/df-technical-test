741db703ef230860e47d3d814ddde1b451a4aa04
jeremyv <jeremy.vain@gmail.com>
2020-02-13 20:38:50 +0100

    Uses `path.resolve` method.

--- client/index.js
+++ client/index.js
@@ -7,13 +7,17 @@ import favicon from "serve-favicon";
 const app = express();
 
 // Middleware for serving a favicon
-app.use(favicon('public/fav.ico'));
+app.use(
+    favicon(path.resolve(__dirname, '../public/favicon.ico'))
+);
 
 // Uses Compression middleware to allow static content to be served with compression
 app.use(compression());
 
 // Serves the content using static directory
-app.use(express.static('public'));
+app.use(
+    express.static(path.resolve(__dirname, '../public'))
+);
 
 // Uses our SSR router
 app.use('/', ssr);
