cbde9d4865db2fe451a4f69f382902973ac3df25
jeremyv <jeremy.vain@gmail.com>
2020-02-14 13:56:26 +0100

    Initializes and configures Sequelize library.

--- config/env.config.js
+++ config/env.config.js
@@ -5,13 +5,13 @@ dotEnv.config();
 export const ENV = {
     NODE_ENV : process.env.NODE_ENV || 'development',
 
-    NODE_CLIENT_HOST : process.env.NODE_CLIENT_HOST || 'localhost',
+    NODE_CLIENT_HOST : process.env.NODE_CLIENT_HOST || '127.0.0.1',
     NODE_CLIENT_PORT : process.env.NODE_CLIENT_PORT || 8080,
 
-    NODE_SERVER_HOST : process.env.NODE_SERVER_HOST || 'localhost',
+    NODE_SERVER_HOST : process.env.NODE_SERVER_HOST || '127.0.0.1',
     NODE_SERVER_PORT : process.env.NODE_SERVER_PORT || 4000,
 
-    DB_HOST     : process.env.DB_HOST || 'localhost',
+    DB_HOST     : process.env.DB_HOST || '127.0.0.1',
     DB_PORT     : process.env.DB_PORT || 3306,
     DB_DATABASE : process.env.DB_DATABASE,
     DB_USER     : process.env.DB_USER,
