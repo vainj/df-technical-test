c9383b01537f454e2cb8605f0cf99ad1bc4658b6
jeremyv <jeremy.vain@gmail.com>
2020-02-14 13:57:01 +0100

    Creates the `users` database migration.

---
+++ database/migrations/20200214115251-create-users.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING(50)
+      },
+      last_name: {
+        type: Sequelize.STRING(50)
+      },
+      email: {
+        type: Sequelize.STRING(100)
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
