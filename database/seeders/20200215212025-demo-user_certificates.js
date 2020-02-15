fcdb3bd2af092056ddab146ba418a9dbcf65e420
jeremyv <jeremy.vain@gmail.com>
2020-02-15 22:28:25 +0100

    Creates `user_certificates` seed.

--- database/seeders/20200215212025-demo-user_certificates.js
+++ database/seeders/20200215212025-demo-user_certificates.js
@@ -2,9 +2,21 @@
 module.exports = {
     up   : (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('user_certificates', [{
-            first_name : 'John',
-            last_name  : 'Doe',
-            email      : 'john.doe@yopmail.com',
+            first_name : 'Dirty',
+            last_name  : 'Gadgette  ',
+            email      : 'dirty.gadgette@yopmail.com',
+            created_at : new Date(),
+            updated_at : new Date()
+        }, {
+            first_name : 'Papi',
+            last_name  : 'Fougasse',
+            email      : 'papi.fougasse@yopmail.com',
+            created_at : new Date(),
+            updated_at : new Date()
+        }, {
+            first_name : 'Ouai',
+            last_name  : 'Ouai',
+            email      : 'ouai.ouai@yopmail.com',
             created_at : new Date(),
             updated_at : new Date()
         }], {});
