bcc23eca632914f2f32b75fb0254ee3887300dcb
jeremyv <jeremy.vain@gmail.com>
2020-02-16 00:08:35 +0100

    Simplifies configuration file content.

--- config/env.config.js
+++ config/env.config.js
@@ -2,23 +2,6 @@
 import * as dotEnv from 'dotenv';
 
 // Loads environment variables from `.env` file
-dotEnv.config();
+const config = dotEnv.config();
 
-export const ENV = {
-    NODE_ENV : process.env.NODE_ENV || 'development',
-
-    NODE_CLIENT_HOST : process.env.NODE_CLIENT_HOST || 'localhost',
-    NODE_CLIENT_PORT : process.env.NODE_CLIENT_PORT || 8080,
-
-    NODE_SERVER_HOST : process.env.NODE_SERVER_HOST || 'localhost',
-    NODE_SERVER_PORT : process.env.NODE_SERVER_PORT || 4000,
-
-    GRAPHQL_ENDPOINT : process.env.GRAPHQL_ENDPOINT,
-
-    DB_HOST     : process.env.DB_HOST || 'localhost',
-    DB_PORT     : process.env.DB_PORT || 3306,
-    DB_DATABASE : process.env.DB_DATABASE,
-    DB_USER     : process.env.DB_USER,
-    DB_PASSWORD : process.env.DB_PASSWORD,
-    DB_DIALECT  : process.env.DB_DIALECT || 'mysql',
-};
+export const ENV = config.parsed;
