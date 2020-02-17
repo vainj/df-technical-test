5eef5965682a12631cb77d4ca73ad1aad2f1b9a4
jeremyv <jeremy.vain@gmail.com>
2020-02-17 08:29:04 +0100

    Updates `user_certificates` seeder.

--- database/seeders/20200215212025-demo-user_certificates.js
+++ database/seeders/20200215212025-demo-user_certificates.js
@@ -5,18 +5,21 @@ module.exports = {
             first_name : 'Dirty',
             last_name  : 'Gadgette  ',
             email      : 'dirty.gadgette@yopmail.com',
+            token      : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTU4MTkyMzE5NywiZXhwIjoxNTgxOTIzMTk5fQ.tS9m74sbUTrLMm0_zBjnK_Wn6EFkj7QT4s25vRI3lVs',
             created_at : new Date(),
             updated_at : new Date()
         }, {
             first_name : 'Papi',
             last_name  : 'Fougasse',
             email      : 'papi.fougasse@yopmail.com',
+            token      : 'dddhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTU4MTkyMzE5NywiZXhwIjoxNTgxOTIzMTk5fQ.tS9m74sbUTrLMm0_zBjnK_Wn6EFkj7QT4s25vRI3lVs',
             created_at : new Date(),
             updated_at : new Date()
         }, {
             first_name : 'Ouai',
             last_name  : 'Ouai',
             email      : 'ouai.ouai@yopmail.com',
+            token      : 'xdahbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTU4MTkyMzE5NywiZXhwIjoxNTgxOTIzMTk5fQ.tS9m74sbUTrLMm0_zBjnK_Wn6EFkj7QT4s25vRI3lVs',
             created_at : new Date(),
             updated_at : new Date()
         }], {});
