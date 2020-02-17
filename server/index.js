00de3a6440feb936e62d0ae10814bee7c301afe9
jeremyv <jeremy.vain@gmail.com>
2020-02-17 23:51:46 +0100

    Creates Email backend service.

--- server/index.js
+++ server/index.js
@@ -1,22 +1,57 @@
 // Imports
 import express from "express";
 import {ApolloServer} from "apollo-server-express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 // App imports
 import {ENV} from "../config";
 import {typeDefsArray} from "./schemas/type_defs";
 import {resolversArray} from "./schemas/resolvers";
 import db from "../database/models";
+import SendEmailService from "./services/mailers/send-email";
 
+const app = express();
+
+// Enable All CORS Requests
+app.use(cors());
+
+/*
+ * GraphQL
+ */
 const dbModels = db.sequelize.models;
 const server   = new ApolloServer({
     typeDefs  : typeDefsArray,
     resolvers : resolversArray,
     context   : {dbModels}
 });
-
-const app = express();
 server.applyMiddleware({app});
+
+// Support json encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+/*
+ * Send email containing the download certificate link
+ */
+app.post('/send', function (req, res) {
+    const sendEmailService = new SendEmailService();
+
+    sendEmailService
+        .certificateFileEmail(
+            req.param('firstName'),
+            req.param('email'),
+            req.param('token')
+        )
+        .then(() => res.json({success : true}))
+        .catch(() => res.json({success : false}));
+});
+
+/*
+ * Generates and download certificate file
+ */
+//TODO
+
 
 // Listen for connections on given port
 app.listen(ENV.NODE_SERVER_PORT);
