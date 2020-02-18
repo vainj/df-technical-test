1e3bbc6b3b38c6f25b9cb06ba3bb3e1836dd7b0e
jeremyv <jeremy.vain@gmail.com>
2020-02-18 13:04:47 +0100

    Generates and download user certificate.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import {ApolloServer} from "apollo-server-express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
+import fs from "fs";
 
 // App imports
 import {ENV} from "../config";
@@ -10,6 +12,7 @@ import {typeDefsArray} from "./schemas/type_defs";
 import {resolversArray} from "./schemas/resolvers";
 import db from "../database/models";
 import SendEmailService from "./services/mailers/send-email";
+import CertificateService from "./services/certificate";
 
 const app = express();
 
@@ -39,9 +42,9 @@ app.post('/send', function (req, res) {
 
     sendEmailService
         .certificateFileEmail(
-            req.param('firstName'),
-            req.param('email'),
-            req.param('token')
+            req.body.firstName,
+            req.body.email,
+            req.body.token
         )
         .then(() => res.json({success : true}))
         .catch(() => res.json({success : false}));
@@ -50,8 +53,33 @@ app.post('/send', function (req, res) {
 /*
  * Generates and download certificate file
  */
-//TODO
+app.get('/download', function (req, res) {
+    // Verifies and decodes given token
+    jwt.verify(
+        req.query.token,
+        ENV.JWT_SECRET,
+        (err, decoded) => {
+            if (err || !decoded || !decoded.hasOwnProperty('fullName') || !decoded.fullName) {
+                res.status(401).send('Invalid given token!');
+            } else {
+                const fullName = decoded.fullName;
 
+                const certificateService = new CertificateService();
+
+                certificateService.generate(fullName)
+                                  .then((generatedFile) => {
+                                      res.download(
+                                          generatedFile,
+                                          () => fs.unlinkSync(generatedFile)
+                                      );
+                                  })
+                                  .catch((error) => {
+                                      res.status(500).send(error);
+                                  });
+            }
+        }
+    );
+});
 
 // Listen for connections on given port
 app.listen(ENV.NODE_SERVER_PORT);
