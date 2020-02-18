82c4e91be2da22eb8fde06e3bbb494ec8cfd5579
jeremyv <jeremy.vain@gmail.com>
2020-02-18 13:11:33 +0100

    Externalizes layout.

--- client/routes/ssr.js
+++ client/routes/ssr.js
@@ -7,6 +7,7 @@ import {SnackbarProvider} from "notistack";
 
 // App imports
 import App from "../components/app";
+import {layout} from "../components/layout";
 
 const router = express.Router();
 
@@ -14,27 +15,6 @@ const router = express.Router();
  * Route to load and display the application
  */
 router.get('/', async (req, res) => {
-    //TODO: Externalizes this HTML!
-    const layout = `
-        <!doctype html>
-        <html lang="en">
-            <head>
-                <title>Certificate generator</title>
-                
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css">
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons|Roboto:300,400,500,700&display=swap">
-            </head>
-            <body>
-                <div id="app">{{{reactDom}}}</div>
-
-                <script src="/app.js" charset="utf-8"></script>
-                <script src="/vendor.js" charset="utf-8"></script>
-            </body>
-        </html>
-    `;
-
     const hbsLayoutTemplate = hbs.compile(layout);
     const reactAppComponent = renderToString(<SnackbarProvider maxSnack={3}><App/></SnackbarProvider>);
     const htmlToDisplay     = hbsLayoutTemplate({reactDom : reactAppComponent});
